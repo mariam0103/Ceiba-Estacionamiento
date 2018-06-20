cbb29aa12cc7527e00604edeebee97559aa01ebf
Maria Alejandra <maria.molina@ceiba.com.co>
2018-06-20 10:15:49 -0500

    master

---
+++ src/test/java/co/com/ceiba/CeibaEstacionamiento/CeibaEstacionamientoApplicationTests.java
@@ -0,0 +1,17 @@
+package co.com.ceiba.CeibaEstacionamiento;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class CeibaEstacionamientoApplicationTests {
+
+	@Test
+	public void sum() {
+		assertTrue(true);
+	}
+	
+}
