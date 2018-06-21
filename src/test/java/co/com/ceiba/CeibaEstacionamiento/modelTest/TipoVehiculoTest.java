abca403a2824ac0bcfb81621db521e80a8836617
Maria Alejandra <maria.molina@ceiba.com.co>
2018-06-21 14:31:38 -0500

    .

--- src/test/java/co/com/ceiba/CeibaEstacionamiento/modelTest/TipoVehiculoTest.java
+++ src/test/java/co/com/ceiba/CeibaEstacionamiento/modelTest/TipoVehiculoTest.java
@@ -3,13 +3,9 @@ package co.com.ceiba.CeibaEstacionamiento.modelTest;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import co.com.ceiba.CeibaEstacionamiento.model.TipoVehiculoModel;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class TipoVehiculoTest {
 	
 	@Test
