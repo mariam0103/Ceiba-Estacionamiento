9e1a2fec1712ce22aa8736c6485622b56915879b
Maria Alejandra <maria.molina@ceiba.com.co>
2018-06-21 13:31:52 -0500

    MASTER

---
+++ src/test/java/co/com/ceiba/CeibaEstacionamiento/modelTest/TipoVehiculoBuilder.java
@@ -0,0 +1,32 @@
+package co.com.ceiba.CeibaEstacionamiento.modelTest;
+
+import co.com.ceiba.CeibaEstacionamiento.model.TipoVehiculoModel;
+
+public class TipoVehiculoBuilder {
+	private Integer idtipo;
+	private String tipo;
+	
+	public TipoVehiculoBuilder(){
+		idtipo = 1;
+		tipo = "CARRO";
+    }
+	
+	public TipoVehiculoBuilder withTipo(String tipo){
+        this.tipo = tipo;
+        return this;
+    }
+    
+    public TipoVehiculoBuilder withIdtipo(Integer idtipo){
+        this.idtipo = idtipo;
+        return this;
+    }
+    
+    public TipoVehiculoModel build(){
+        return new TipoVehiculoModel(idtipo, tipo);
+    }
+    
+    public static TipoVehiculoBuilder anTipoVehiculo(){
+        return new TipoVehiculoBuilder();
+    }
+    
+}
---
+++ src/test/java/co/com/ceiba/CeibaEstacionamiento/modelTest/TipoVehiculoTest.java
@@ -0,0 +1,18 @@
+package co.com.ceiba.CeibaEstacionamiento.modelTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import co.com.ceiba.CeibaEstacionamiento.model.TipoVehiculoModel;
+
+public class TipoVehiculoTest {
+	
+	@Test
+	public void TipoVehiculoModel() {
+		TipoVehiculoModel tipoVehiculo = TipoVehiculoBuilder.anTipoVehiculo().withTipo("CARRO").withIdtipo(1).build();
+		assertEquals("CARRO", tipoVehiculo.getTipo());
+		assertEquals("1", tipoVehiculo.getIdtipo().toString());
+    }
+		
+}
