1e7b08f2b59a28f25f055ce13387b9555eb5ae17
Maria Alejandra <maria.molina@ceiba.com.co>
2018-06-22 07:55:29 -0500

    test unitario

--- src/test/java/co/com/ceiba/CeibaEstacionamiento/CeibaEstacionamientoApplicationTests.java
+++ src/test/java/co/com/ceiba/CeibaEstacionamiento/CeibaEstacionamientoApplicationTests.java
@@ -1,12 +1,6 @@
 package co.com.ceiba.CeibaEstacionamiento;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 public class CeibaEstacionamientoApplicationTests {
 
